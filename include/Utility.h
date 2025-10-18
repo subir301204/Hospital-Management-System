ac55f01d8841def592760bf88e9cccbb512aae1e
subir301204 <thunderalpha509@gmail.com>
2025-10-18 10:48:30 +0530

    Updating the indention of the Utility files & adding a new function for getting hiden inputs from consol

--- include/Utility.h
+++ include/Utility.h
@@ -5,16 +5,32 @@
 #include <iostream>
 #include <fstream>
 #include <vector>
+#include <string>
+#include <conio.h>
 
 using namespace std;
 
 // File handling functions
 void saveToFile(const vector<Patient> &data);
 
+// Function to load patieent details to the program from the data/data.txt file
 vector<Patient> loadFromFile();
 
+/*
+ * Funtion to search Patients in the system
+ * It searches by taking the data base and patient id
+*/
 int searchPatient(const vector<Patient> &data, int id);
 
+/*
+ * Function to search Patients in the system
+ * It sesarches by only taking the data base
+*/
 int searchPatient(vector<Patient> &data);
+
+// Function to get hidden inputs
+string getHiddenInput();
+
+// Function to create passwords
 
 #endif
